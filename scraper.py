7a03528dc12c984cd8a9af253662b98ae2f45f09
Holly Becker <becker.holly@gmail.com>
2022-06-07 11:11:26 -0700

    Parse the protobuf from the album page and write out
    Easier comparisons when making changes

--- scraper.py
+++ scraper.py
@@ -1,43 +1,45 @@
 #!/usr/bin/env python3
 import argparse
+import json
 import re
 
 import requests
 from bs4 import BeautifulSoup
 
 
-def filter_bg_image(attr):
-    return attr and re.compile('background-image: url').search(attr)
-
-
-def main(album_url):
+def main(album_url, protobuf_output):
     print(f"Fetching {album_url}")
-    # Get URL?
+
+    # Get album
     try:
         response = requests.get(album_url)
     except Exception:
         print(f"Error fetching {album_url}")
         return
 
-    soup = BeautifulSoup(response.text, 'lxml')
-    # print(soup)
-    # Title: tag.name = title
-    for tag in soup.find_all(True):
-        print('TAG HERE', tag.name)
-        if tag.name not in ('script', 'html', 'head', 'base', 'meta', 'link', 'style', 'body', 'svg', 'defs', 'radialgradient'):
-            print(tag.name)
-        if tag.name == 'div':
-            print(tag.attrs)
-    # for tag in soup.find_all(style=filter_bg_image):
-    #     print(tag)
-    # This isn't giving me the same thing I'm seeing in a browser
-    # Look at manually saving as har file, using haralyzer package to work with it
+    print("Parsing response")
+    soup = BeautifulSoup(response.text, features="html.parser")
+
+    # find the spot where the protobuf is defined
+    target = soup.find_all(string=re.compile("^AF_initDataCallback"))[0]
+    start = target.find("[")
+    end = target.rfind("]") + 1
+
+    # load the protobuf to json
+    protobuf = json.loads(target[start:end])
+
+    print(f"found protobuf, writing to {protobuf_output}")
+
+    # write to file
+    with open(protobuf_output, "w") as f:
+        json.dump(protobuf, f, indent=4)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        "Scrapes Google Photos into static HTML page with images")
-    parser.add_argument(
-        'album_url', help='URL to Google photos album to scrape')
+        "Scrapes Google Photos into static HTML page with images"
+    )
+    parser.add_argument("album_url", help="URL to Google photos album to scrape")
+    parser.add_argument("protobuf_output", help="file to write")
     args = parser.parse_args()
     main(**vars(args))
