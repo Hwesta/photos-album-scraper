d868b856dd5be71a49c42b52d6a4c34a182478a9
Holly Becker <becker.holly@gmail.com>
2022-06-07 16:07:13 -0700

    Refactor scraper to use Album class

--- scraper.py
+++ scraper.py
@@ -7,32 +7,46 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def main(album_url, protobuf_output):
-    print(f"Fetching {album_url}")
+class Album:
+    """Handle fetching and parsing a Google Photo album"""
 
-    # Get album
-    try:
-        response = requests.get(album_url)
-    except Exception:
-        print(f"Error fetching {album_url}")
-        return
+    PROTOBUF_REGEX = r"^AF_initDataCallback"
 
-    print("Parsing response")
-    soup = BeautifulSoup(response.text, features="html.parser")
+    def __init__(self, album_url):
+        self.album_url = album_url
+        self.soup = None
+        self.protobuf = None
 
-    # find the spot where the protobuf is defined
-    target = soup.find_all(string=re.compile("^AF_initDataCallback"))[0]
-    start = target.find("[")
-    end = target.rfind("]") + 1
+    def get_album(self, parser="html.parser"):
+        """Fetch album from URL, parse to protobuf"""
+        print(f"Fetching {self.album_url}")
 
-    # load the protobuf to json
-    protobuf = json.loads(target[start:end])
+        try:
+            response = requests.get(self.album_url)
+        except Exception:
+            print(f"Error fetching {self.album_url}")
+            return
 
-    print(f"found protobuf, writing to {protobuf_output}")
+        print(f"Parsing response with {parser}")
+        self.soup = BeautifulSoup(response.text, features=parser)
 
-    # write to file
-    with open(protobuf_output, "w") as f:
-        json.dump(protobuf, f, indent=4)
+        # Find the spot where the protobuf is defined
+        target = self.soup.find_all(string=re.compile(self.PROTOBUF_REGEX))[0]
+        start = target.find("[")
+        end = target.rfind("]") + 1
+
+        # Load the protobuf to json. If this works we probably have the right thing
+        self.protobuf = json.loads(target[start:end])
+        print("Found protobuf")
+
+    def write_protobuf(self, protobuf_output):
+        """Write the protobuf as formatted JSON."""
+        print(f"Writing protobuf to {protobuf_output}")
+        if self.protobuf is None:
+            raise RuntimeError("Must run get_album() first")
+
+        with open(protobuf_output, "w") as f:
+            json.dump(self.protobuf, f, indent=4)
 
 
 if __name__ == "__main__":
@@ -40,6 +54,13 @@ if __name__ == "__main__":
         "Scrapes Google Photos into static HTML page with images"
     )
     parser.add_argument("album_url", help="URL to Google photos album to scrape")
-    parser.add_argument("protobuf_output", help="file to write")
+    parser.add_argument(
+        "--protobuf-output", help="Write the raw protobuf output to this file, if given"
+    )
     args = parser.parse_args()
-    main(**vars(args))
+
+    album = Album(args.album_url)
+    album.get_album()
+
+    if args.protobuf_output:
+        album.write_protobuf(args.protobuf_output)
